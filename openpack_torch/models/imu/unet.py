b0d3c2b78a4c24ea7cf8c537ae8d570a963ad394
lly1117 <92363379+lly1117@users.noreply.github.com>
2022-07-26 00:18:26 +0900

    Update unet.py

--- openpack_torch/models/imu/unet.py
+++ openpack_torch/models/imu/unet.py
@@ -10,20 +10,12 @@ from functools import partial
 
 nonlinearity = partial(F.relu,inplace=True)
 
-def conv2d(filter_in, filter_out, kernel_size, stride=1):
-    pad = (kernel_size - 1) // 2 if kernel_size else 0
-    return nn.Sequential(OrderedDict([
-        ("conv", nn.Conv2d(filter_in, filter_out, kernel_size=kernel_size, stride=stride, padding=pad, bias=False)),
-        ("bn", nn.BatchNorm2d(filter_out)),
-        ("relu", nn.LeakyReLU(0.1)),
-    ]))
-
 class Upsample(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Upsample, self).__init__()
 
         self.upsample = nn.Sequential(
-            conv2d(in_channels, out_channels, 1),
+            nn.conv2d(in_channels, out_channels, 1),
             nn.Upsample(scale_factor=2, mode='nearest')
         )
 
